468e44999e6b961611c977edb18295aa48f331a4
Hpday <>
2026-03-31 22:18:36 +0800

    test(Pinia): 测试 Pinia 的持久化用例

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from './stores/index'
+</script>
 
 <template>
   <h1>App</h1>
+  <p>{{ useUserStore().token }}</p>
+  <button @click="useUserStore().setToken('Hello World')">修改Token</button>
+  <button @click="useUserStore().removeToken()">清除Token</button>
   <RouterView></RouterView>
 </template>
 
