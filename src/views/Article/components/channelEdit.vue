13619e704f6b7565982ddc5b3c4b51f636ff0be9
Hpday <>
2026-04-15 14:57:31 +0800

    feat: 创建弹层表单数据 - 使用 el-form 创建表单 - 准备表单数据与校验规则 (:model='formData', :rules='rules') - 绑定表单数据与校验规则 (v-model='formData.xxx', prop='xxx') - 基于 open 方法传递的数据进行表单的渲染 - 有数据的是编辑，无数据的是添加，据此对 dialog 标题进行控制

--- src/views/Article/components/channelEdit.vue
+++ src/views/Article/components/channelEdit.vue
@@ -4,16 +4,52 @@ import { ref } from 'vue'
 const dialogVisible = ref(false)
 // 组件方法
 const open = (obj) => {
-  console.log(obj)
+  // console.log(obj)
+  formData.value = { ...obj }
   dialogVisible.value = true
 }
 defineExpose({
   open
 })
+// 表单数据
+const formData = ref({
+  cate_name: '',
+  cate_alias: ''
+})
+const rules = {
+  cate_name: [
+    { required: true, message: '请输入分类名称', trigger: 'blur' },
+    {
+      pattern: /^\s{1,10}$/,
+      message: '分类名称必须是1-10位非空字符',
+      trigger: 'blur'
+    }
+  ],
+  cate_alias: [
+    { required: true, message: '请输入分类名称', trigger: 'blur' },
+    {
+      pattern: /^[a-zA-Z0-9]$/,
+      message: '分类名称必须是1-15位字母或数字',
+      trigger: 'blur'
+    }
+  ]
+}
 </script>
 <template>
-  <el-dialog v-model="dialogVisible" title="标题" width="500">
-    <span>内容</span>
+  <el-dialog v-model="dialogVisible" :title="formData.id ? '编辑分类' : '添加分类'" width="500">
+    <el-form
+      :model="formData"
+      :rules="rules"
+      label-width="100px"
+      style="padding-right: 30px"
+    >
+      <el-form-item label="分类名称" prop="cate_name">
+        <el-input v-model="formData.cate_name" />
+      </el-form-item>
+      <el-form-item label="分类别名" prop="cate_alias">
+        <el-input v-model="formData.cate_alias" />
+      </el-form-item>
+    </el-form>
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
